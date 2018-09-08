017f5691144358b9397cacbd658f5afbea11a4f7
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2018-09-08 21:30:39 +0530

    Simple Fron Interface Updated

---
+++ Interface(V1.0)/src/lk/ijse/rrs/controller/DashbordController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lk.ijse.rrs.controller;
+
+//import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Sithum Ravishara
+ */
+public class DashbordController implements Initializable {
+    
+    
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
